c1b9053ad94d748c73e4adb22eb48f4c95c457d5
Chesh1re <n.butakov98@gmail.com>
2025-08-27 11:41:46 +0300

    created Patient, Employer and Conclusion models

---
+++ KDL/employer/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+class BaseModel(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+class Department(BaseModel):
+    class Meta:
+        db_table = 'Department'
+
+
+class JT(BaseModel):
+    class Meta:
+        db_table = 'JT'
+
+
+class Role(BaseModel):
+    class Meta:
+        db_table = 'Role'
+
+
+class Employer(models.Model):
+    s_name = models.CharField(max_length=50, null=True)
+    name = models.CharField(max_length=50, null=True)
+    surname = models.CharField(max_length=50, null=True)
+    dep = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+    jt = models.ForeignKey(JT, on_delete=models.SET_NULL, null=True)
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        db_table = 'Employer'
+
+
+class AD(models.Model):
+    emp = models.ForeignKey(Employer, on_delete=models.SET_NULL, null=True)
+    login = models.CharField(max_length=50, null=True)
+    pwd = models.CharField(max_length=50, null=True)
+
+    class Meta:
+        db_table = 'AD'
---
+++ KDL/patient/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Patient(models.Model):
+    s_name = models.CharField(max_length=50, null=True)
+    name = models.CharField(max_length=50, null=True)
+    surname = models.CharField(max_length=50, null=True)
+    date_birth = models.DateField(null=True)
+    p_series = models.CharField(max_length=50, null=True)
+    p_number = models.CharField(max_length=50, null=True)
+    snils = models.CharField(max_length=50, null=True)
+    med_polis = models.CharField(max_length=50, null=True)
+    medstat_id = models.CharField(max_length=50, null=True)
+
+    class Meta:
+        db_table = 'Patient'
+
+
+class Research(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.SET_NULL, null=True)
+    date = models.DateField(null=True)
+    research_name = models.CharField(max_length=50, null=True)
+    data = models.TextField(null=True)
+
+    class Meta:
+        db_table = 'Research'
+
+
+
+
