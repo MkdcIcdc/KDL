82446d2dc9a3dad21a8ca3dcc859e71b109dafcb
Chesh1re <n.butakov98@gmail.com>
2025-09-02 16:22:46 +0300

    add gender field for Patient models

--- KDL/patient/models.py
+++ KDL/patient/models.py
@@ -4,6 +4,7 @@ class Patient(models.Model):
     s_name = models.CharField(max_length=50, null=True)
     name = models.CharField(max_length=50, null=True)
     surname = models.CharField(max_length=50, null=True)
+    gender = models.CharField(max_length=20, null=True)
     date_birth = models.DateField(null=True)
     p_series = models.CharField(max_length=50, null=True)
     p_number = models.CharField(max_length=50, null=True)
