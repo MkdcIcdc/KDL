b98a2676500bc562b93b139306fe1b22ada2f448
Chesh1re <n.butakov98@gmail.com>
2025-09-03 09:30:26 +0300

    add view and serializer for list of patients

--- KDL/patient/views.py
+++ KDL/patient/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
 from .models import Patient
-from .serializer import PatientSerializer
+from .serializer import PatientSerializer, PatientsListSerializer
 
 
 class PatientViewSet(viewsets.ModelViewSet):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+
+
+class PatientsListViewSet(viewsets.ModelViewSet):
+    queryset = Patient.objects.all()
+    serializer_class = PatientsListSerializer
