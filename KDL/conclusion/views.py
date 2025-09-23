f8935f9e131aa25316a4fccf72dd3aa9d946905d
Chesh1re <n.butakov98@gmail.com>
2025-09-23 13:48:03 +0300

    from Shamil func for interpriter lab_data

--- KDL/conclusion/views.py
+++ KDL/conclusion/views.py
@@ -3,8 +3,11 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Conclusion
 from .serializer import ConclusionSerializer
+from lab_data_interpreter import interpreter
+from dotenv import load_dotenv
+import os
 
-
+load_dotenv(".env")
 class ConclusionViewSet(viewsets.ModelViewSet):
     queryset = Conclusion.objects.all()
     serializer_class = ConclusionSerializer
@@ -12,6 +15,17 @@ class ConclusionViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['post'])
     def run_function(self, request):
         param = request.data.get('param')
+        interpreter.getState_by_reseach_id_and_save_to_base(
+            param_db={
+                'host': os.getenv('DB_HOST'),
+                'port': os.getenv('DB_PORT'),
+                'database': os.getenv('DB_NAME'),
+                'user': os.getenv('DB_USER'),
+                'password': os.getenv('DB_PASSWORD'),
+                'client_encoding': 'utf8',
+            },
+            research_id=param
+        )
         try:
             result = f'Good! Param {param}'
             return Response({
