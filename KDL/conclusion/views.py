a052e03d6390c74a3f2c048152f1c64c411cf27f
Chesh1re <n.butakov98@gmail.com>
2025-09-24 10:23:49 +0300

    some comm

--- KDL/conclusion/views.py
+++ KDL/conclusion/views.py
@@ -39,11 +39,11 @@ class ConclusionViewSet(viewsets.ModelViewSet):
 
             interpreter.getState_by_reseach_id_and_save_to_base(
                 param_db={
-                    'host': os.getenv('POSTGRES_HOST'),
-                    'port': os.getenv('POSTGRES_PORT'),
-                    'database': os.getenv('POSTGRES_DB'),
-                    'user': os.getenv('POSTGRES_USER'),
-                    'password': os.getenv('POSTGRES_PASSWORD'),
+                    'host': os.getenv('DB_HOST'),
+                    'port': os.getenv('DB_PORT'),
+                    'database': os.getenv('DB_NAME'),
+                    'user': os.getenv('DB_USER'),
+                    'password': os.getenv('DB_PASSWORD'),
                     'client_encoding': 'utf8',
                 },
                 research_id=param
