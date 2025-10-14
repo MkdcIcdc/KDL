58a4b16f5e96d146deb62e16c400059151d34287
Chesh1re <n.butakov98@gmail.com>
2025-10-14 09:35:43 +0300

    add passport data for researched patient

--- KDL/db2_worker/serializer.py
+++ KDL/db2_worker/serializer.py
@@ -4,7 +4,11 @@ from .models import SearchPatient
 
 class SearchPatientSerializer(serializers.ModelSerializer):
     date_birth = serializers.DateField(format='%d.%m.%Y')
+    full_passport_data = serializers.SerializerMethodField()
 
     class Meta:
         model = SearchPatient
-        fields = ['id', 's_name', 'name', 'surname', 'date_birth', 'medstat_id']
+        fields = ['id', 's_name', 'name', 'surname', 'date_birth', 'full_passport_data', 'medstat_id']
+
+    def get_full_passport_data(self, obj):
+        return f'{obj.p_series} {obj.p_number}'
